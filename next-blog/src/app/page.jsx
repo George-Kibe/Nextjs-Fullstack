cd3869139d37dc1e72994c4a46c9cb1ea9d25ac5
George-Kibe <georgekibewambui@gmail.com>
2023-06-04 18:44:48 +0300

    home page

--- next-blog/src/app/page.jsx
+++ next-blog/src/app/page.jsx
@@ -1,9 +1,21 @@
-
+import Image from "next/image";
+import HeroImage from "public/hero.png"
 
 export default function Home() {
   return (
-   <div>
-    Home
+   <div className="flex flex-col md:flex-row items-center gap-5">
+    <div className="flex-1 flex flex-col gap-2 md:gap-5 items-center">
+      <h1 className="font-bold text-[25px] md:text-[50px]">
+        Better design for your digital products.
+      </h1>
+      <p className="text">
+        Turning your idea into a reality. We bring together teams from the tech industry.
+      </p>
+      <button className="p-2 cursor-pointer bg-[#53c28b] border-none rounded-md text-white w-[50%]">See Our Works</button>
+    </div>
+    <div className="flex-1">
+      <Image src={HeroImage} alt="HeroImage" className="w-full h-full object-cover" />
+    </div>
    </div>
   )
 }
--- next-blog/src/components/footer/Footer.jsx
+++ next-blog/src/components/footer/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
     <div className='text-{14px] flex items-center justify-between mb-1'>
       <div>©2023 Buenas Group. All Rights Reserved</div>
       <div className='flex gap-2'>
-        <div className='h-5 w-5 relative'><Image fill className='cursor-pointer opacity-50' src={"/1.png"} alt='Buenas Facebook' /></div>
-        <div className='h-5 w-5 relative'><Image fill className='cursor-pointer opacity-50' src={"/2.png"} alt='Buenas Instagram' /></div>
-        <div className='h-5 w-5 relative'><Image fill className='cursor-pointer opacity-50' src={"/3.png"} alt='Buenas Twitter' /></div>
-        <div className='h-5 w-5 relative'><Image fill className='cursor-pointer opacity-50' src={"/4.png"} alt='Buenas Youtube' /></div>
+        <div className='h-5 w-5 relative'><Image fill className='cursor-pointer opacity-90' src={"/1.png"} alt='Buenas Facebook' /></div>
+        <div className='h-5 w-5 relative'><Image fill className='cursor-pointer opacity-90' src={"/2.png"} alt='Buenas Instagram' /></div>
+        <div className='h-5 w-5 relative'><Image fill className='cursor-pointer opacity-90' src={"/3.png"} alt='Buenas Twitter' /></div>
+        <div className='h-5 w-5 relative'><Image fill className='cursor-pointer opacity-90' src={"/4.png"} alt='Buenas Youtube' /></div>
       </div>
     </div>
   )
