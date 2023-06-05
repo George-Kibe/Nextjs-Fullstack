0e99ed673151efae2196d11c8bfd68a02ec012ac
George-Kibe <georgekibewambui@gmail.com>
2023-06-05 12:49:39 +0300

    posts APIs

--- next-blog/next.config.js
+++ next-blog/next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
         domains: [
             'images.pexels.com',
             'buenasapp-images-bucket161901-dev.s3.eu-west-1.amazonaws.com',
-            'buenare-images-bucket.s3.eu-west-1.amazonaws.com'
+            'buenare-images-bucket.s3.eu-west-1.amazonaws.com',
+            'buenas-portfolio-bucket.s3.eu-west-1.amazonaws.com'
         ]
     }
 }
--- next-blog/src/app/blog/page.jsx
+++ next-blog/src/app/blog/page.jsx
@@ -3,25 +3,27 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-async function getData() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+async function getBlogsData() {
+  const res = await fetch("http://localhost:3000/api/posts", {
     // next: { revalidate:10}, revalidate after every 10 seconds
-    cache: "no-store"
+    cache: "no-store",
   })
-  if(!res.ok) {
-    throw new Error("Failed to fetch Data!")
-  }
+  console.log("Response", res)
+  // if(!res.ok) {
+  //   throw new Error("Failed to fetch Data!")
+  // }
   return res.json();
 }
 
 const Blog = async() => {
-  const blogs = await getData();
+  const blogs = await getBlogsData();
+  console.log(blogs)
   return (
     <div className="">
       <h1 className=""></h1>
       {/* Loop through all blogs and show their images, title and short description */}
       {
-        blogs.map((blog) => (
+        blogs && blogs.map((blog) => (
           <div className="flex flex-col md:flex-row items-center mb-4" key={blog.id}>
             <Link href={`/blog/${blog.id}`} className="md:mr-10 relative h-64 w-64 md:h-80 md:w-80">
               <Image src={blog.image||"/illustration.png"} alt="Contact Image" fill className='object-contain'/>
